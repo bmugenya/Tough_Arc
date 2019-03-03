a4e89ccf0fae5bb3f59c1f0d491ed20fd5b004fa
bmugenya26@gmail.com <bmugenya26@gmail.com>
2019-03-03 17:30:25 +0300

    [Feature #164355046] added blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,11 @@
 from flask import Flask
 from flask_restful import Api, Resource
 
-from .api.v1.views import Share, Buy, View, AdminReg, UserReg, AdminAccess, UserAccess
+from .api.v1 import version_one as v1
 
 
 def create_app():
     app = Flask(__name__)
-    api = Api(app)
-    api.add_resource(Share, "/book/share")
-    api.add_resource(View, "/book/book_id")
-    api.add_resource(Buy, "/books")
-    api.add_resource(AdminReg, "/auth/register/admin")
-    api.add_resource(AdminAccess, "/auth/login/admin")
-    api.add_resource(UserReg, "/auth/register/user")
-    api.add_resource(UserAccess, "/auth/login/user")
+    app.register_blueprint(v1)
 
     return app
