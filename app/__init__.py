e83b5dc0ef0510089aad54542613df5e3966cf0e
bmugenya26@gmail.com <bmugenya26@gmail.com>
2019-03-03 17:15:42 +0300

    [Feature #164355046] modularised the codebase

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,18 @@
+from flask import Flask
+from flask_restful import Api, Resource
 
+from .api.v1.views import Share, Buy, View, AdminReg, UserReg, AdminAccess, UserAccess
+
+
+def create_app():
+    app = Flask(__name__)
+    api = Api(app)
+    api.add_resource(Share, "/book/share")
+    api.add_resource(View, "/book/book_id")
+    api.add_resource(Buy, "/books")
+    api.add_resource(AdminReg, "/auth/register/admin")
+    api.add_resource(AdminAccess, "/auth/login/admin")
+    api.add_resource(UserReg, "/auth/register/user")
+    api.add_resource(UserAccess, "/auth/login/user")
+
+    return app
---
+++ app/api/v1/views.py
@@ -0,0 +1,141 @@
+from flask import jsonify, make_response, request
+from flask_restful import Resource
+
+
+from models import *
+
+
+class Share(Resource, Books):
+    def __init__(self):
+        self.book = Books()
+
+    def post(self):
+        data = request.get_json()
+        book_id = data['id']
+        postedOn = data['postedOn']
+        postedBy = data['postedBy']
+        title = data['title']
+        lecture = data['lecture']
+        condition = data['condition']
+        comment = data['comment']
+
+        newBook = self.book.share(book_id, postedOn, postedBy, title, lecture, condition, comment)
+
+        return make_response(jsonify({
+            "message": "Book added succedfully",
+            "data": newBook
+
+        }), 201)
+
+
+class Buy(Resource, BuyBook):
+    def __init__(self):
+        self.book = BuyBook()
+
+    def post(self):
+        data = request.get_json()
+        book_id = data['id']
+        title = data['title']
+        lecture = data['lecture']
+        condition = data['condition']
+
+        book = self.book.buy(book_id, title, lecture, condition)
+
+        return make_response(jsonify({
+            "message": "Book added succedfully",
+            "data": book
+
+        }), 201)
+
+
+class View(Resource, ViewBook):
+    def __init__(self):
+        self.book = ViewBook()
+
+    def get(self):
+        book = self.book.get_books()
+
+        return make_response(jsonify({
+            "message": "Book added succedfully",
+            "data": book
+
+        }), 200)
+
+
+class AdminReg(Resource, AdminRegistration):
+    def __init__(self):
+        self.user = AdminRegistration()
+
+    def post(self):
+        data = request.get_json()
+        student_id = data['student_id']
+        firstName = data['firstName']
+        password = data['password']
+        lastName = data['lastName']
+        email = data['email']
+        username = data['username']
+
+        admin = self.user.save_admin(student_id, firstName, password, lastName, email, username)
+
+        return make_response(jsonify({
+            "message": "user added succedfully",
+            "admin": admin
+
+        }), 201)
+
+
+class UserReg(Resource, UserRegistration):
+    def __init__(self):
+        self.user = UserRegistration()
+
+    def post(self):
+        data = request.get_json()
+        student_id = data['student_id']
+        firstName = data['firstName']
+        password = data['password']
+        lastName = data['lastName']
+        email = data['email']
+        username = data['username']
+
+        user = self.user.save_users(student_id, firstName, password, lastName, email, username)
+
+        return make_response(jsonify({
+            "message": "user added succedfully",
+            "admin": user
+        }), 201)
+
+
+class AdminAccess(Resource, AdminLogin):
+    def __init__(self):
+        self.user = AdminLogin()
+
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+
+        admin = self.user.login(username, password)
+
+        return make_response(jsonify({
+            "message": "user added succedfully",
+            "admin": admin
+
+        }), 200)
+
+
+class UserAccess(Resource, UserLogin):
+    def __init__(self):
+        self.user = UserLogin()
+
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+
+        user = self.user.login(username, password)
+
+        return make_response(jsonify({
+            "message": "user added succedfully",
+            "admin": user
+
+        }), 200)
