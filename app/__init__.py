2c884a2610f87e14b76df59d62be40a00dbb5c1f
bmugenya26@gmail.com <bmugenya26@gmail.com>
2019-03-04 22:55:25 +0300

    [Feature #164362230] imported database module

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, Blueprint
 
 from .api.v1 import version_one as v1
+from db_con import database_setup
 
 
 def create_app():
     app = Flask(__name__)
+    database = database_setup()
+    database.create_tables()
     app.register_blueprint(v1)
 
     return app
