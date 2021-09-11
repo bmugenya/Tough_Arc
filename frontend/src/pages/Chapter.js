44c58b0f83b6b757a242f3bf38b4c049b5ffe22f
Bmugenya <bmugenya26@gmail.com>
2021-09-11 23:34:23 +0300

    added pagination

--- frontend/src/pages/Chapter.js
+++ frontend/src/pages/Chapter.js
@@ -1,15 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import data from '../utils/data'
+import images from '../utils/chapter.json'
+import Pagination from '../components/pagination';
 import IconButton from '@material-ui/core/IconButton'
 import '../assets/css/Chapter.css'
 import { useHistory } from 'react-router-dom'
+import { useMemo } from 'react';
 
+
+let PageSize = 1;
 const Chapter = () => {
   const history = useHistory()
+  console.log(data[0].chapters)
   const [chapter, setChapter] = useState(data)
   const [isError, setIsError] = useState(false)
+    const [currentPage, setCurrentPage] = useState(1);
+
   const { p_id } = useParams()
+  const currentTableData = useMemo(() => {
+
+
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return images.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+
 
   useEffect(() => {
     const getChapter = () => {
@@ -26,6 +42,7 @@ const Chapter = () => {
 
   return (
     <>
+
       <section className='container'>
         {isError && <div>Something went wrong ...</div>}
         {chapter.map((item) => {
@@ -38,14 +55,21 @@ const Chapter = () => {
                 </IconButton>
               ))}
 
-              {Object.keys(chapters).map((detail) => (
+              {currentTableData.map((chapter) => (
                 <div className='img-container'>
-                  <img src={chapters[detail]} alt={title} />
+                  <img src={chapter.content} alt={title} />
                 </div>
               ))}
             </article>
           )
         })}
+          <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={images.length}
+        pageSize={PageSize}
+        onPageChange={page => setCurrentPage(page)}
+      />
       </section>
     </>
   )
